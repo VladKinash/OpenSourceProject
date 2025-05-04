80f995cfa875a4d37b06f0db2b330e750334083a
Syrmpopoulos Stavros <stsirbp@gmail.com>
2025-05-04 21:24:12 +0300

    signup functionality

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
+const mysql = require('mysql2');
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine','ejs');
 app.use(express.json());
 app.use(express.static('views'));
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'OpenSourceProject',
+});
 
+const db = pool.promise();
 const port = 1000;
+
+app.listen(port, () => {
+    console.log("Server run on port "+ port);
+})
+
+
 
 app.get('/', (req, res) => {
     res.redirect('/login');
@@ -24,10 +37,25 @@ app.get('/signup', (req, res) => {
     res.render('pages/signup');
 })
 
+app.post('/signup', async (req, res) => {
+    try {
+        const username = req.body.username; 
+        const [users] = await db.query('SELECT * FROM users WHERE username='+username)
+        if (users.length == 0){
+            if (req.body.password == req.body.re_password){
+                const [result] = await db.execute('INSERT INTO users (username, password) VALUES ('+username+', '+req.body.password+')');
+            } else {
+                res.render('/signup', {"error": "passwords do not match"});
+            }
+        } else {
+            res.render('/signup', {"error": "user already exists"});
+        }
+    } catch (error){
+        console.log(error);
+    }
+})
+
 // Game Page
 
 // Lobby ??
 
-app.listen(port, () => {
-    console.log("Server run on port "+ port);
-})
---
+++ opensourceproject.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: May 04, 2025 at 06:22 PM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `opensourceproject`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username_unique` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
+(1, 'user1', 'user1', 'player'),
+(2, 'user2', 'user2', 'admin');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
